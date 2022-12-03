0df7b9cb1ea20312eb735767b9c08ebe1f348789
xyzuan <xyzuannihboss@gmail.com>
2022-12-03 19:01:44 +0100

    Blissify: [SQUASH] User toggle for unlimited photos storage
    fixup! Settings: mnmlist: User toggle for unlimited photos storage [2/2]
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>

--- src/org/blissroms/blissify/fragments/misc/Misc.java
+++ src/org/blissroms/blissify/fragments/misc/Misc.java
@@ -57,9 +57,13 @@ public class Misc extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private SwitchPreference mGamesSpoof;
+    private SwitchPreference mPhotosSpoof;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -69,6 +73,10 @@ public class Misc extends SettingsPreferenceFragment implements
         mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
         mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
         mGamesSpoof.setOnPreferenceChangeListener(this);
+
+        mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -81,6 +89,10 @@ public class Misc extends SettingsPreferenceFragment implements
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
